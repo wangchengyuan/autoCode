cdbdb5f89d5e66f41b1bc70a36bebdd9f17eaaa6
wangchengyuan <784444045@qq.com>
2017-02-22 16:11:16 +0800

    添加生成HTML报告程序

--- runTest.py
+++ runTest.py
@@ -1,10 +1,22 @@
 import unittest
 from bin.sendMail import sendMail
 from bin.configRead import configRead
+import os
+from HTMLTestRunner import HTMLTestRunner
+
 
 testDir='./case/'
 discover=unittest.defaultTestLoader.discover(testDir,'test*.py')
 
 if __name__ == '__main__':
-    mail=sendMail()
-    mail.send_mail()
+   # mail=sendMail()
+    #mail.send_mail()
+   filename="test"
+   fp=open(filename,"wb")
+   runner=HTMLTestRunner(stream=fp,title='测试报告',description='用例执行情况：')
+   runner.run(discover)
+   fp.close()
+
+   #提取最新的测试报告
+
+   #发送测试报告
